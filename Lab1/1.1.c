f02bf82eab932f4494ab777ba828a615717413c8
SUNNY SAGAR <sunnysagar620@gmail.com>
2021-10-22 13:08:21 +0530

    Add files via upload

---
+++ Lab1/1.1.c
@@ -0,0 +1,16 @@
+digit [0-9]
+alpha [A-Za-z]
+
+%%
+{digit}+ {printf("Digit.");}
+{alpha}+ {printf("Alphabet.");}
+.* {printf("Invalid.");}
+%%
+
+int main(){
+    yylex();
+    return 0;
+}
+int yywrap(void){
+}
+
