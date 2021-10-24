a199e03f11422175b115e77aed94a0ac13f85c07
Sunny <sunnysagar620@gmail.com>
2021-10-24 15:15:13 +0530

    add the floder

---
+++ Lab 8/1.cpp
@@ -0,0 +1,18 @@
+
+
+digit [0-9]
+alpha [A-Za-z]
+
+%%
+{digit}+ {printf("Digit.");}
+{alpha}+ {printf("Alphabet.");}
+.* {printf("Invalid.");}
+%%
+
+int main(){
+    yylex();
+    return 0;
+}
+int yywrap(void){
+}
+
